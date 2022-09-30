7d3092a021f23ae9d456f53d3a9af5a76564e19f
kateusacova <kateusacova@gmail.com>
2022-09-30 12:45:39 +0100

    Project setup

---
+++ spec/posts_tags_seeds.sql
@@ -0,0 +1,17 @@
+TRUNCATE posts, tags, posts_tags RESTART IDENTITY;
+
+INSERT INTO "public"."posts" ("id", "title") VALUES
+(1, 'How to use Git'),
+(2, 'Ruby classes');
+
+
+INSERT INTO "public"."posts_tags" ("post_id", "tag_id") VALUES
+(1, 1),
+(2, 1),
+(2, 3);
+
+
+INSERT INTO "public"."tags" ("id", "name") VALUES
+(1, 'coding'),
+(2, 'travel'),
+(3, 'cooking');
